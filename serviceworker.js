b5a0171a6e53445c87537e012282711a9a3e7477
muzammil-umar-cd <muzammil.coredigitals@gmail.com>
2024-12-23 21:01:51 +0500

    Revert "repatch-reverse"
    This reverts commit 276ef9aa4208ba4f379711e61b2eb8400ba3eb7a.

--- serviceworker.js
+++ serviceworker.js
@@ -1,14 +1,14 @@
-// var staticCacheName = "the-shop-pwa-" + new Date().getTime();
-// var filesToCache = [
-//     '/public/web-assets/img/icons/icon-72x72.png',
-//     '/public/web-assets/img/icons/icon-96x96.png',
-//     '/public/web-assets/img/icons/icon-128x128.png',
-//     '/public/web-assets/img/icons/icon-144x144.png',
-//     '/public/web-assets/img/icons/icon-152x152.png',
-//     '/public/web-assets/img/icons/icon-192x192.png',
-//     '/public/web-assets/img/icons/icon-384x384.png',
-//     '/public/web-assets/img/icons/icon-512x512.png',
-// ];
+var staticCacheName = "the-shop-pwa-" + new Date().getTime();
+var filesToCache = [
+    '/public/web-assets/img/icons/icon-72x72.png',
+    '/public/web-assets/img/icons/icon-96x96.png',
+    '/public/web-assets/img/icons/icon-128x128.png',
+    '/public/web-assets/img/icons/icon-144x144.png',
+    '/public/web-assets/img/icons/icon-152x152.png',
+    '/public/web-assets/img/icons/icon-192x192.png',
+    '/public/web-assets/img/icons/icon-384x384.png',
+    '/public/web-assets/img/icons/icon-512x512.png',
+];
 
 // // Cache on install
 // self.addEventListener("install", event => {
