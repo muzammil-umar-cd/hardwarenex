b1c5b5379770a6664ad0d73c594736a78d1b1e2c
muzammil-umar-cd <muzammil.coredigitals@gmail.com>
2024-12-25 01:12:33 +0500

    order-single-update

--- app/Http/Resources/OrderSingleCollection.php
+++ app/Http/Resources/OrderSingleCollection.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources;
 
 use App\Http\Resources\OrderProductCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Address;
+use Illuminate\Support\Facades\Request as FacadeRequest;
 
 class OrderSingleCollection extends JsonResource
 {
@@ -15,7 +17,25 @@ class OrderSingleCollection extends JsonResource
      */
     public function toArray($request)
     {   
-        
+        if(!auth('api')->user()){
+            $get_user_address = Address::where('ip_address','=',FacadeRequest::ip())->first();
+
+            return [
+                'id' => $this->id,
+                'code' => $this->code,
+                'user' => [
+                    'name' => $get_user_address->full_name,
+                    'email' => $get_user_address->email,
+                    'phone' => $get_user_address->phone,
+                    'avatar' => 'None',
+                ],
+                'shipping_address' => json_decode($this->shipping_address),
+                'billing_address' => json_decode($this->billing_address),
+                'grand_total' => (double) $this->grand_total,
+                'orders' => OrderResource::collection($this->orders),
+                'date' => $this->created_at->toFormattedDateString()
+            ];
+        }
         return [
             'id' => $this->id,
             'code' => $this->code,
